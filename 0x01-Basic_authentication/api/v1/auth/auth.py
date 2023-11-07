cc5be94f31038ae502e7f26efdadfa4869bd8bc2
Monisola Keshiro <106781966+Monisola17@users.noreply.github.com>
2023-11-07 23:26:38 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python3
-"""Authentication module.
+""" API authentication
 """
 from flask import request
 from typing import List, TypeVar
-import fnmatch
 
 
-class Auth:
-    """Authentication class.
-    """
+class Auth():
+    """ manages the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if auth is required.
-        """
-        if path is None:
+        """ require authorithation check"""
+        if path is None or excluded_paths is None or not len(excluded_paths):
             return True
-
-        if excluded_paths is None or not excluded_paths:
-            return True
-
-        for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
-                return False
-
-        return True
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if p.endswith('*'):
+                if path.startswith(p[:1]):
+                    return False
+        return False if path in excluded_paths else True
 
     def authorization_header(self, request=None) -> str:
-        """ Method to get authorization header.
-        """
-        if request is not None:
-            return request.headers.get('Authorization', None)
-        return None
+        """ authorization header check"""
+        if request:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Method to get user from request.
-        """
+        """ current user method"""
         return None
